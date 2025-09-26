0d40067b2f619f268e29b9cd07f5ba09c088b40c
Shiv Vyas <shivyas217@gmail.com>
2025-09-26 15:09:06 -0500

    This is for the events page and the profile creation page.
    I only added the src files that are needed.

---
+++ shiv-vyas/src/App.tsx
@@ -0,0 +1,14 @@
+import "./css/App.css";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div className="main-area">
+      <div className="profile">
+        <Profile />
+      </div>
+    </div>
+  );
+}
+
+export default App;
